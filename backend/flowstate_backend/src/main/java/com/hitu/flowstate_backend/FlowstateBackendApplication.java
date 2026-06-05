422af6e93f299b7bbfa33125127c69c1d99405a7
Hitaishi Posanpeta <hitaishi@vt.edu>
2026-06-05 16:23:00 -0400

    springboot init

---
+++ backend/flowstate_backend/src/main/java/com/hitu/flowstate_backend/FlowstateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hitu.flowstate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowstateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowstateBackendApplication.class, args);
+    }
+
+}
---
+++ backend/flowstate_backend/src/test/java/com/hitu/flowstate_backend/FlowstateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitu.flowstate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowstateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
